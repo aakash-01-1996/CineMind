b95d179fb3d26c526c6c09bf3b48c497f0e93fa0
aakash <aakashambodkar99@gmail.com>
2024-03-07 16:52:48 -0500

    Completed Sign-In/Sign-Up Form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className="absolute px-5 py-2 m-5 bg-gradient-to-t from-black">
+    <div className="absolute  px-5 py-2 m-5 bg-gradient-to-t from-black shadow-lg z-20">
      <img 
      className=" w-44"
      alt="netflixLogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1597px-Netflix_2015_logo.svg.png?20190206123158" />  
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);  
+  }
   return <div>
     <Header />
       
-      <div>
-        <img alt="logo" src="https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/fa6f97d9-245e-43d7-bb56-af27cbf6d656/US-en-20240226-popsignuptwoweeks-perspective_alpha_website_large.jpg" />
+      <div className="absolute"> 
+        <img alt="logo" 
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/fa6f97d9-245e-43d7-bb56-af27cbf6d656/US-en-20240226-popsignuptwoweeks-perspective_alpha_website_large.jpg" 
+        />
       </div>
+
+      <form className="w-3/12 h-auto p-12 bg-black absolute my-36 mx-auto right-0 left-0 rounded-lg text-white bg-opacity-80">
+        <h1 className="font-bold text-3xl py-6"> {isSignInForm? "Sign In" : "Sign Up"}</h1>
+        {!isSignInForm && <input 
+        type="text" 
+        placeholder=" First name" 
+        className="p-3 my-3 text-white w-full bg-gray-700 rounded-md"
+        />}
+
+        {!isSignInForm && <input 
+        type="text" 
+        placeholder=" Last name" 
+        className="p-3 my-3 text-white w-full bg-gray-700 rounded-md"
+        />}
+
+        <input 
+          type="text" 
+          placeholder=" Email or phone number" 
+          className=" flex p-3 my-3 text-white w-full bg-gray-700 rounded-md"
+          />
+         
+        <input 
+        type="password" 
+        placeholder=" Password" 
+        className="p-3 my-3 text-white w-full bg-gray-700 rounded-md"
+        />
+       {!isSignInForm && <input 
+        type="password" 
+        placeholder="Confirm Password" 
+        className="p-3 my-3 text-white w-full bg-gray-700 rounded-md"
+        />}
+        <button className="p-3 my-6 rounded-md bg-red-700 text-white w-full">
+        {isSignInForm? "Sign In" : "Sign Up"}
+          </button>
+          <p className="py-4 cursor-pointer " 
+          onClick={toggleSignInForm}>
+            {isSignInForm? "  New to Netflix? Sign up now." : "Already a member? Sign In"}
+          
+            </p>
+      </form> 
   </div>;
 };
 
