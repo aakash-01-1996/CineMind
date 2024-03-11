655b070a42649ac2ebf02f4b96ceb1e91c4c017d
aakash <aakashambodkar99@gmail.com>
2024-03-11 19:44:20 -0400

    Browse Movies

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,7 +41,7 @@ const Header = () => {
 }, [])
 
   return (
-    <div className="absolute w-full px-5 py-2 m-5 bg-gradient-to-t from-black shadow-lg z-20 flex justify-between">
+    <div className="absolute w-full px-5 py-2 m-5 from-black z-50 flex justify-between">
      <img 
      className=" w-44"
      alt="netflixLogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1597px-Netflix_2015_logo.svg.png?20190206123158" />  
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -96,7 +96,7 @@ const Login = () => {
     <Header />
       
       <div className="absolute"> 
-        <img alt="logo" 
+        <img alt="backgroundImg" 
         src="https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/fa6f97d9-245e-43d7-bb56-af27cbf6d656/US-en-20240226-popsignuptwoweeks-perspective_alpha_website_large.jpg" 
         />
       </div>
